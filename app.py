c738934d86270a256b28a684ce37dad1243a90f9
Bri9k <Bri9k@protonmail.com>
2019-05-12 11:52:24 +0530

    got bare bones app working, added requirements

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 app.config.from_pyfile('config.py')
 
-@app.route('/')
-def hello_world():
-    return "Hello World!"
+@app.route('/', methods = ['POST'])
+def main_screen():
+    return render_template("index.html")
+
+@app.route('/result', methods = ['POST'])
+def narcissism_recommend():
+    name = request.form['inputusername']
+    return_stuff = [(name, 1) for i in range(10)]
+    return render_template('result.html', recommends = return_stuff)
